99c0788df8ee6b077ab91933fa18b4dbe00ecff2
rnahill <rei333lly@gmail.com>
2023-12-11 10:54:03 -0500

    added id to db.json and added some comments

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+// Consts needed
 const express = require('express');
-
 const path = require('path');
-
 const app = express();
 
 const PORT = 3001;
 
+
+// Middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get
+
+// hw setup
+
+// look for fetches in public folder and take note of the routes
+
+// fetch('/api/notes', {
+//     method: 'GET',
+
+//app.get('/api/notes', (req,res) => {}
+
+
+// fetch('/api/notes', {
+//     method: 'POST',
+
+// app.post('/api/notes', (req,res) => {}
+
+
+//   fetch(`/api/notes/${id}`, {
+// method: 'DELETE',
+
+//app.delete('/api/notes/:note_id', (req,res) => {}
