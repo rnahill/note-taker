ffa251e421918a2ab80d773dfa03123f56fa9391
rnahill <rei333lly@gmail.com>
2023-12-13 14:30:56 -0500

    added beginning get routes

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // API routes
 
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "public", 'index.html'));
+
+});
+
+app.get('/notes', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "public", 'notes.html'));
+    
+});
+
 app.get('/api/notes', (req,res) => {
     fs.readFile('./db/db.json', (err, data) =>{
         if (err) {
