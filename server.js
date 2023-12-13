7eb9cd5b8cb890f06b0d1ac31215d517a38518e3
rnahill <rei333lly@gmail.com>
2023-12-13 14:35:17 -0500

    test server and reformatting

--- server.js
+++ server.js
@@ -16,18 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// API routes
-
 app.get('/', (req, res) => {
-
     res.sendFile(path.join(__dirname, "public", 'index.html'));
-
 });
 
 app.get('/notes', (req, res) => {
-
     res.sendFile(path.join(__dirname, "public", 'notes.html'));
-    
 });
 
 app.get('/api/notes', (req,res) => {
